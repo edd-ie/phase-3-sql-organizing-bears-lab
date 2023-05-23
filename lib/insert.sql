6d09d2925417ae0538ed636c9d2b04fd5338cb3d
edd-ie <ogendoeddie@gmail.com>
2023-05-23 16:34:47 +0300

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive)
+VALUES(1, "Mr. Chocolate", 12, 'M', 'brown', 'calm', 1),
+    (2, "Rowdy", 12, 'M', 'brown', 'calm', 0),
+    (3, "Tabitha", 12, 'F', 'brown', 'calm', 1),
+    (4, "Sergeant Brown", 12, 'M', 'brown', 'calm', 1),
+    (5, "Melissa", 12, 'F', 'brown', 'calm', 0),
+    (6, "Grinch", 12, 'M', 'brown', 'calm', 1),
+    (7, "Wendy", 12, 'F', 'brown', 'calm', 1),
+    (8, null, 12, 'F', 'brown', 'calm', 1);
